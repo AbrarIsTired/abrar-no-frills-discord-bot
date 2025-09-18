4719b177e51f208160d3bea144ff0fa735c2ab76
AbrarIsTired <abraramn@udel.edu>
2025-09-17 23:18:10 -0400

    Added basic command. Added basic message check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# Abrar's Discord Bot
+# Abrar's Discord Bot: M4 SOPMOD II
+
+# Bot character based on the mobile gacha game, Girl's Frontline in where a character is named M4-SOPMOD II. Art is also from said game
 
 #Imports
 import discord
@@ -17,17 +19,44 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-#Prefix is m! for M4 SOPMOD II, my personal bot
-bot = commands.Bot(command_prefix = "m!", activity=discord.Game('with IEDs'),intents=intents)
+#Declaring Prefix as m! and setting Playing Status 
+bot = commands.Bot(command_prefix = "m!", activity=discord.Game('with LEGOs'),intents=intents)
 
-@bot.command
+#Launch Status in the Terminal
+@bot.event
 async def on_ready():
     print(f"Heyo! {bot.user.name} Reporting In!")
 
 
-#Connection of the bot using f-strings
-@bot.command()
+#Prefix Based Commands (ex. m!command)
+
+#Latency of the Bot
+@bot.command
 async def ping(ctx):
     await ctx.send(f'Current Latency: {round(bot.latency * 1000)}ms')
 
+
+#Context Based Commands (ex. Triggers on specific words outside of command)
+
+#Word-Counter
+count = 0
+@bot.event
+async def on_message(message):
+    global count
+    if(message.author == bot.user):
+        return None
+    
+    if("m4" in message.content.lower()):
+        await message.channel.send(f"Hey, you said my name! Hello {message.author.mention}")
+        return None
+    
+    word_bank = ["hello", "python"]
+    for i in word_bank:
+        if(i in message.content.lower()):
+            count += 1
+            await message.channel.send(f"Hey, you said {i}! That's from the word-bank. Total Count: {count}.")
+            break
+
+
+#Using Bot Token from the enviroment file it runs the bot
 bot.run(discord_token)
